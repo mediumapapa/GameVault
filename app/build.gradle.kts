696948f2fa0e8a01a1e507792e8e6e0397bd56ed
Santiago Guerrero <guerrero.colin.santiago@gmail.com>
2026-06-08 16:06:31 -0600

    Arreglando problemas de renderizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.gamevault"
+        applicationId = "com.mediumapapa.gamevault"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
--- app/src/androidTest/java/com/example/gamevault/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/gamevault/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.gamevault", appContext.packageName)
+        assertEquals("com.mediumapapa.gamevault", appContext.packageName)
     }
 }
